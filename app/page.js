237a958396f759c7a881a3dae01224d2e324e77f
Jake Shankman <81862032+JakeS-dot@users.noreply.github.com>
2024-05-13 01:02:04 +0000

    committed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react"
-import DancingLines from "react-dancing-lines/index.es.js"
+// import DancingLines from "react-dancing-lines/index.es.js"
 import Image from "next/image";
 function wrapLetters(text, id = null) {
   return text.split("").map((char, index) => {
@@ -44,7 +44,7 @@ export default function Home() {
       <main>
 
         <div id={"home"}>
-          <DancingLines></DancingLines>
+          {/* <DancingLines></DancingLines> */}
           <div className="title">
             <div className="title-flex1">
               <p>{wrapLetters("Hello,", "hello")}</p>
