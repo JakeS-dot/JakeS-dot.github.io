f10366547459333377bfcd4b23d92aee937eecc3
JakeS-dot <s9158257@dekalbschoolsga.org>
2024-05-13 22:59:08 -0400

    Added about me text and skills title, i need to fix all these !important tags soon btw!

--- app/page.js
+++ app/page.js
@@ -44,16 +44,16 @@ export default function Home() {
           {/* <DancingLines></DancingLines> */}
           <div className="title">
             <div className="title-flex1">
-              <p>{wrapLetters("Hello,", "hello")}</p>
-              <p>
+              <p className={'main-title'}>{wrapLetters("Hello,", "hello")}</p>
+              <p className={'main-title'}>
                 {wrapLetters("I'm ")}
                 <span className={"marque-overlay"}>
                   {wrapLetters("Jake Shankman", "title-marque")}
                 </span>
               </p>
-              <p>{wrapLetters("A Web Developer", "smaller-title")}</p>
-              <p id="title-desc">Specializing in Next.js and React</p>
-              <p id={"title-button-wrapper"}>
+              <p className={'main-title'}>{wrapLetters("A Web Developer", "smaller-title")}</p>
+              <p id={"title-desc"} className={'main-title'}>Specializing in Next.js and React</p>
+              <p className={'main-title'} id={"title-button-wrapper"}>
                 <a href={"#contact"} id="title-button">
                   Contact Me!
                 </a>
@@ -63,11 +63,42 @@ export default function Home() {
         </div>
         <section id="about">
           <br/><br/><br/>
-          <div className="title" id={"title-about-wrapper"}>
-            <p id={"title-about"}>About Me</p>
+          <div className="title non-landing-title" >
+            <p id={"title-about"} className={'main-title'}>About Me</p>
+            <div>
+              <p className={'main-title sub-title-text'}>
+                As a highly motivated student developer with four years of
+                experience in Python coding and web development languages, I
+                have gained valuable skills in building APIs and designing
+                HTML/CSS interfaces. Starting my coding journey at the age of
+                nine, I have demonstrated a strong passion for technology and a
+                dedication to continuous learning. <br />
+                <br />
+                My proficiency in Python has enabled me to develop efficient
+                algorithms and create functional applications, while my
+                knowledge of API development has allowed me to design flexible
+                and scalable interfaces. Additionally, my expertise in HTML/CSS
+                has allowed me to create visually appealing and user-friendly
+                web pages. <br /> <br />
+               <div className="no-mobile">Motivated by a strong desire to broaden my horizons and stay
+                 ahead of the technological curve, I am currently focused on
+                 expanding my skill set to include other programming languages.
+                 Through dedication, perseverance, and a willingness to step
+                 outside my comfort zone, I am confident that I can master new
+                 tools and technologies and excel in any programming endeavor
+                 that comes my way.</div>
+              </p>
+            </div>
           </div>
         </section>
-        <section id="skills"></section>
+        <section id="skills">
+          <br/><br/><br/>
+          <div className="title non-landing-title">
+            <p className="main-title" id="title-skills">
+              Skills
+            </p>
+          </div>
+        </section>
         <section id="work"></section>
         <section id="contact"></section>
       </main>
