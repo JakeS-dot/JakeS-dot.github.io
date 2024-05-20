4174bc43a77fc4c579ed8ce9ff1c3f06fbdf8721
JakeS-dot <s9158257@dekalbschoolsga.org>
2024-05-20 19:23:23 -0400

    Added some more stuff

--- app/drake/page.js
+++ app/drake/page.js
@@ -2,18 +2,26 @@ import Image from "next/image";
 
 export default function Drake() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        "background-color": "black",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <Image src={"/drake.png"} alt={"drake"} width={500} height={500}></Image>
-      <h1>This is for you evan pookie :) You cant click the vod btw</h1>
-    </div>
+      <>
+          <h1 style={{display: "flex", backgroundColor: "black", textAlign: 'center', padding: 5}}>This is for you evan pookie :) You cant click the vod btw</h1>
+          <div
+              style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "black",
+                  width: "100vw",
+                  height: "100vh",
+              }}
+          >
+              <Image
+                  src={"/drake.png"}
+                  alt={"drake"}
+                  width={500}
+                  height={500}
+              ></Image>
+          </div>
+
+      </>
   );
 }
